9b6fd2705deb24aee2cfcbe4e44de50568375d25
IanConsulting <martoxdlol@gmail.com>
2023-05-03 17:30:05 -0300

    astro config (preact, tailwind, node server)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind(), preact()],
+  adapter: node({
+    mode: "standalone"
+  })
+});
